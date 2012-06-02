b4498621a2b1bad62af5b8ae82995b4ce924afd9
unknown <technocake@random.(none)>
2012-06-03 01:06:17 +0200

    After a heavy session on linking and shared libraries, I synthesized a mindmap on helpful resources for this, and C in general.

---
+++ C.mm
@@ -0,0 +1,49 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1338677558964" ID="ID_244624164" MODIFIED="1338677612906" TEXT="C">
+<node CREATED="1338677635704" FOLDED="true" ID="ID_865196630" MODIFIED="1338678261193" POSITION="right" TEXT="Coding Style">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1338677658202" ID="ID_980748942" LINK="http://www.teamten.com/lawrence/style/notes.html" MODIFIED="1338677661817" TEXT="http://www.teamten.com/lawrence/style/notes.html"/>
+<node CREATED="1338677678809" ID="ID_1046862363" LINK="http://queue.acm.org/detail.cfm?id=1039535" MODIFIED="1338677698366" TEXT="How Not to Write Fortran in Any Language5"/>
+</node>
+<node CREATED="1338677719016" FOLDED="true" ID="ID_1580555610" MODIFIED="1338678258889" POSITION="left" TEXT="linking">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1338677731728" FOLDED="true" ID="ID_1705496434" MODIFIED="1338678009612" TEXT="Dynamic Linking">
+<arrowlink DESTINATION="ID_1184045180" ENDARROW="Default" ENDINCLINATION="422;0;" ID="Arrow_ID_212420346" STARTARROW="None" STARTINCLINATION="422;0;"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1338677741303" ID="ID_1321783553" LINK="http://tldp.org/HOWTO/Program-Library-HOWTO/dl-libraries.html" MODIFIED="1338677755820" TEXT="at runtime"/>
+</node>
+<node CREATED="1338677998242" FOLDED="true" ID="ID_135946848" MODIFIED="1338678043801" TEXT="Static Linking">
+<node CREATED="1338678003866" ID="ID_1184045180" LINK="http://www.yolinux.com/TUTORIALS/LibraryArchives-StaticAndDynamic.html" MODIFIED="1338677990057" TEXT="http://www.yolinux.com/TUTORIALS/LibraryArchives-StaticAndDynamic.html"/>
+</node>
+<node CREATED="1338678062998" ID="ID_127033472" MODIFIED="1338678066139" TEXT="library">
+<node CREATED="1338678079021" ID="ID_1710092938" LINK="http://www.adp-gmbh.ch/cpp/gcc/create_lib.html" MODIFIED="1338678086291" TEXT="Creating a shared and static library "/>
+</node>
+<node CREATED="1338678102661" ID="ID_1877327220" LINK="http://www.iecc.com/linker/linker10.html" MODIFIED="1338678120037" TEXT="Troughfull guide"/>
+</node>
+<node CREATED="1338677797367" FOLDED="true" ID="ID_700608531" MODIFIED="1338677809710" POSITION="right" TEXT="Beginers guide">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1338677802958" ID="ID_606524516" LINK="http://www.programmingsimplified.com/c-program-examples" MODIFIED="1338677807362" TEXT="http://www.programmingsimplified.com/c-program-examples"/>
+</node>
+<node CREATED="1338677818862" FOLDED="true" ID="ID_1512354002" MODIFIED="1338678258321" POSITION="left" TEXT="References">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1338677822541" ID="ID_1262847365" LINK="http://www.gnu.org/software/libc/manual/pdf/libc.pdf" MODIFIED="1338677983875" TEXT="gnu libc">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1338677853494" ID="ID_10568123" LINK="http://www.gnu.org/software/libc/manual/" MODIFIED="1338677859217" TEXT="Several versions"/>
+</node>
+<node CREATED="1338678126925" ID="ID_333556491" LINK="http://www.iecc.com/" MODIFIED="1338678249898" TEXT="not sure what"/>
+</node>
+<node CREATED="1338677889092" FOLDED="true" ID="ID_802149124" MODIFIED="1338678260506" POSITION="right" TEXT="Books">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1338677892980" ID="ID_1693419110" LINK="http://publications.gbdirect.co.uk/c_book/" MODIFIED="1338677911791" TEXT="The C Book">
+<node CREATED="1338677901172" ID="ID_1154368692" LINK="http://publications.gbdirect.co.uk/c_book/the_c_book.pdf" MODIFIED="1338677903608" TEXT="PDF"/>
+</node>
+<node CREATED="1338677927820" ID="ID_334673525" LINK="http://www.cs.cf.ac.uk/Dave/C/CE.html" MODIFIED="1338677933295" TEXT="Programming in C"/>
+</node>
+<node CREATED="1338677963411" FOLDED="true" ID="ID_1355602456" MODIFIED="1338677980067" POSITION="left" TEXT="Tutorials">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1338677968603" ID="ID_193541463" LINK="http://www.itk.ntnu.no/ansatte/Hendseth_Sverre/c/lec1.html" MODIFIED="1338677975715" TEXT="Course at NTNU"/>
+</node>
+</node>
+</map>
